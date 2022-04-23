0482a49cfabc4e553a8f65ea3a73a6d5e0572c53
camilagsoares <camila.soares19@hotmail.com>
2022-04-23 01:18:58 -0300

    Start of project created

---
+++ login-page/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  <TheForm />
+  </div>
+</template>
+
+<script>
+import TheForm from './components/TheForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheForm
+  }
+}
+</script>
+
+<style>
+
+</style>
